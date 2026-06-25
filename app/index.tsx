ef9c4530aff29b2e712d0b3c99b38d5472b8ff10
jmcodizal <jessamcodizal.com>
2026-06-25 12:04:44 +0800

    Remove old React Native files

--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Redirect } from 'expo-router';
-
-import { useAuth } from '@/app/auth';
-
-export default function Index() {
-  const { isAuthenticated } = useAuth();
-
-  return <Redirect href={isAuthenticated ? '/monitor' as any : '/login'} />;
-}
